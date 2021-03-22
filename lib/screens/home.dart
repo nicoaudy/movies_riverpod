05a142df02d5e24b49b22c5131cd99aff16b635a
Nico Audy <nicoaudy1994@gmail.com>
2021-03-22 14:29:47 +0700

    called the movie through screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movies_riverpod/models/movie_model.dart';
+import 'package:movies_riverpod/services/movie_service.dart';
 
-class HomeScreen extends StatelessWidget {
+final movieFutureProvider =
+    FutureProvider.autoDispose<List<MovieModel>>((ref) async {
+  ref.maintainState = true;
+
+  final movieService = ref.read(movieServiceProvider);
+  final movies = await movieService.getMovies();
+  return movies;
+});
+
+class HomeScreen extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text("Movies"),
+      ),
+      body: watch(movieFutureProvider).when(
+        loading: () => Center(child: CircularProgressIndicator()),
+        error: (e, s) {
+          return Text("Error");
+        },
+        data: (movies) {
+          return GridView.extent(
+            maxCrossAxisExtent: 200,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12,
+            childAspectRatio: 0.7,
+            children: movies.map((movie) => Text(movie.title)).toList(),
+          );
+        },
+      ),
+    );
   }
 }
